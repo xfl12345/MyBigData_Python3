ee585a809a8ab812bbe248ade32b1f98ddc9b5e1
xfl12345 <xfl1046539849@qq.com>
2021-01-10 23:47:11 +0800

    诈尸更新

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MyBigData_Python3
+
+软件环境：**Python 3.8.5**
+
--- my_pooled_db_monitor.py
+++ my_pooled_db_monitor.py
@@ -1,9 +1,20 @@
 import my_pooled_db
 
-the_pool = my_pooled_db.connection_pool
-while(True):
-    print("connections count=",len(the_pool._connections))
-    print("shared_cache count=",len(the_pool._shared_cache))
-    print("idle_cache count=",len(the_pool._idle_cache))
+class monitor:
+    the_pool = None
+    _running = True
+
+    def __init__(self):
+        self.the_pool = my_pooled_db.connection_pool
+        self._running = True
+
+    def run(self,**arg):
+        while(self._running):
+            print("connections count=",len(self.the_pool._connections))
+            print("shared_cache count=",len(self.the_pool._shared_cache))
+            print("idle_cache count=",len(self.the_pool._idle_cache))
+
+    def stop(self):
+        self._running = False
 
     
