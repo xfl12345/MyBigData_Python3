99b1d99535f84568654ec32e007a0680cbbd6941
xfl12345 <xfl1046539849@qq.com>
2021-06-01 19:12:07 +0800

    更新注释

--- mybigdata/appconst/data_type_lenght.py
+++ mybigdata/appconst/data_type_lenght.py
@@ -1,8 +1,8 @@
 from . import data_type
 
-# 长度单位： Byte
+# mysql 5.7的数据类型长度范围，长度单位： Byte
 mysql5p7 = {
-    # 固定长度的类型
+    # 固定长度的数据类型
     "fixed": {
         data_type.Mysql5p7.TINYINT: 1,
         data_type.Mysql5p7.SMALLINT: 2,
@@ -20,21 +20,23 @@ mysql5p7 = {
         data_type.Mysql5p7.DATETIME: 8,
         data_type.Mysql5p7.TIMESTAMP: 4,
     },
+    # 可变长度的数据类型（范围可以用 固定的最小值 和 固定的最大值 来表示的）
     "variable": {
-        data_type.Mysql5p7.CHAR: (0, (1 << 8) -1),
-        data_type.Mysql5p7.VARCHAR: (0, (1 << 16) -1),
+        data_type.Mysql5p7.CHAR: (0, (1 << 8) - 1),
+        data_type.Mysql5p7.VARCHAR: (0, (1 << 16) - 1),
 
-        data_type.Mysql5p7.TINYBLOB: (0, (1 << 8) -1),
-        data_type.Mysql5p7.BLOB: (0, (1 << 16) -1),
-        data_type.Mysql5p7.MEDIUMBLOB: (0, (1 << 24) -1),
-        data_type.Mysql5p7.LONGBLOB: (0, (1 << 32) -1),
+        data_type.Mysql5p7.TINYBLOB: (0, (1 << 8) - 1),
+        data_type.Mysql5p7.BLOB: (0, (1 << 16) - 1),
+        data_type.Mysql5p7.MEDIUMBLOB: (0, (1 << 24) - 1),
+        data_type.Mysql5p7.LONGBLOB: (0, (1 << 32) - 1),
 
-        data_type.Mysql5p7.TINYTEXT: (0, (1 << 8) -1),
-        data_type.Mysql5p7.TEXT: (0, (1 << 16) -1),
-        data_type.Mysql5p7.MEDIUMTEXT: (0, (1 << 24) -1),
-        data_type.Mysql5p7.LONGTEXT: (0, (1 << 32) -1),
+        data_type.Mysql5p7.TINYTEXT: (0, (1 << 8) - 1),
+        data_type.Mysql5p7.TEXT: (0, (1 << 16) - 1),
+        data_type.Mysql5p7.MEDIUMTEXT: (0, (1 << 24) - 1),
+        data_type.Mysql5p7.LONGTEXT: (0, (1 << 32) - 1),
 
     },
+    # 特殊的可变长度的数据类型
     "special-variable": {
         data_type.Mysql5p7.DECIMAL: ((1, 65), (0, 30))
     },
