8b4b374a32991fe28b2c19ed8c4f6dc5f6452a38
xfl12345 <xfl1046539849@qq.com>
2021-06-05 13:36:41 +0800

    研究JSON Schema

---
+++ mybigdata/src/test/testJsonSchema.py
@@ -0,0 +1,22 @@
+import json
+import jschon
+
+
+with open("./test.json", "r", encoding="utf-8") as f:
+    testJsonStr = json.loads(f.read())
+print(testJsonStr)
+
+
+with open("../main/resources/json/schema/base_request_object.json", "r", encoding="utf-8") as f:
+    jsonSchemaStr = json.loads(f.read())
+print(jsonSchemaStr)
+
+catalogue = jschon.Catalogue.create_default_catalogue("2020-12")
+apiJsonSchema = jschon.JSONSchema(jsonSchemaStr).validate()
+jsonObject = jschon.JSON(testJsonStr)
+
+print( jsonObject )
+print(apiJsonSchema.evaluate( jsonObject ).valid)
+
+
+
