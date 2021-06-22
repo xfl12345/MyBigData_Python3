90af3f07db7f9f0479056cc12aa1452d3b1dfaed
xfl12345 <xfl1046539849@qq.com>
2021-06-22 19:40:57 +0800

    使用Python类代替Python字典

--- mybigdata/src/main/appconst/data_type_class.py
+++ mybigdata/src/main/appconst/data_type_class.py
@@ -1,47 +1,70 @@
+import json
+from mybigdata.src.main.utils.encoder import MyJSONEncoder
 from . import data_type
 
-# mysql 5.7的数据类型分类
-mysql5p7 = {
+
+class BaseStruct(object):
     # 整数类型（集合）
-    "number": {
-        data_type.Mysql5p7.TINYINT,
-        data_type.Mysql5p7.SMALLINT,
-        data_type.Mysql5p7.MEDIUMINT,
-        # data_type.Mysql5p7.INT,
-        data_type.Mysql5p7.INTEGER,
-        data_type.Mysql5p7.BIGINT,
-    },
+    number = None
     # 使用IEEE 754标准的浮点数类型（集合）
-    "IEEE754": {
-        data_type.Mysql5p7.FLOAT,
-        data_type.Mysql5p7.DOUBLE,
-    },
+    IEEE754 = None
     # 时间类型（集合）
-    "time": {
-        data_type.Mysql5p7.DATE,
-        data_type.Mysql5p7.TIME,
-        data_type.Mysql5p7.YEAR,
-        data_type.Mysql5p7.DATETIME,
-        data_type.Mysql5p7.TIMESTAMP,
-    },
+    time = None
     # 字符串类型（集合）
-    "string": {
-        data_type.Mysql5p7.CHAR,
-        data_type.Mysql5p7.VARCHAR,
-
-        data_type.Mysql5p7.TINYBLOB,
-        data_type.Mysql5p7.BLOB,
-        data_type.Mysql5p7.MEDIUMBLOB,
-        data_type.Mysql5p7.LONGBLOB,
-
-        data_type.Mysql5p7.TINYTEXT,
-        data_type.Mysql5p7.TEXT,
-        data_type.Mysql5p7.MEDIUMTEXT,
-        data_type.Mysql5p7.LONGTEXT,
-    },
+    string = None
     # 特殊类型（集合)
-    "special": {
-        data_type.Mysql5p7.DECIMAL,
-    },
+    special = None
 
-}
+    def to_json_string(self):
+        return json.dumps(self, cls=MyJSONEncoder, sort_keys=True, indent=4)
+
+
+# mysql 5.7的数据类型分类
+class MySQL5p7(BaseStruct):
+    def __init__(self):
+        BaseStruct.__init__(self)
+        # 整数类型（集合）
+        self.number = {
+            data_type.Mysql5p7.TINYINT,
+            data_type.Mysql5p7.SMALLINT,
+            data_type.Mysql5p7.MEDIUMINT,
+            # data_type.Mysql5p7.INT,
+            data_type.Mysql5p7.INTEGER,
+            data_type.Mysql5p7.BIGINT,
+        }
+        # 使用IEEE 754标准的浮点数类型（集合）
+        self.IEEE754 = {
+            data_type.Mysql5p7.FLOAT,
+            data_type.Mysql5p7.DOUBLE,
+        }
+        # 时间类型（集合）
+        self.time = {
+            data_type.Mysql5p7.DATE,
+            data_type.Mysql5p7.TIME,
+            data_type.Mysql5p7.YEAR,
+            data_type.Mysql5p7.DATETIME,
+            data_type.Mysql5p7.TIMESTAMP,
+        }
+        # 字符串类型（集合）
+        self.string = {
+            data_type.Mysql5p7.CHAR,
+            data_type.Mysql5p7.VARCHAR,
+
+            data_type.Mysql5p7.TINYBLOB,
+            data_type.Mysql5p7.BLOB,
+            data_type.Mysql5p7.MEDIUMBLOB,
+            data_type.Mysql5p7.LONGBLOB,
+
+            data_type.Mysql5p7.TINYTEXT,
+            data_type.Mysql5p7.TEXT,
+            data_type.Mysql5p7.MEDIUMTEXT,
+            data_type.Mysql5p7.LONGTEXT,
+        }
+        # 特殊类型（集合)
+        self.special = {
+            data_type.Mysql5p7.DECIMAL,
+        }
+
+
+# 单例
+mysql5p7 = MySQL5p7()
--- mybigdata/src/main/appconst/data_type_lenght.py
+++ mybigdata/src/main/appconst/data_type_lenght.py
@@ -1,45 +1,61 @@
+import json
+from mybigdata.src.main.utils.encoder import MyJSONEncoder
 from . import data_type
 
-# mysql 5.7的数据类型长度范围，长度单位： Byte
-mysql5p7 = {
+class BaseStruct:
     # 固定长度的数据类型
-    "fixed": {
-        data_type.Mysql5p7.TINYINT: 1,
-        data_type.Mysql5p7.SMALLINT: 2,
-        data_type.Mysql5p7.MEDIUMINT: 3,
-        data_type.Mysql5p7.INTEGER: 4,
-        # data_type.Mysql5p7.INT: 4,
-        data_type.Mysql5p7.BIGINT: 8,
-
-        data_type.Mysql5p7.FLOAT: 4,
-        data_type.Mysql5p7.DOUBLE: 8,
-
-        data_type.Mysql5p7.DATE: 3,
-        data_type.Mysql5p7.TIME: 3,
-        data_type.Mysql5p7.YEAR: 1,
-        data_type.Mysql5p7.DATETIME: 8,
-        data_type.Mysql5p7.TIMESTAMP: 4,
-    },
+    fixed = None
     # 可变长度的数据类型（范围可以用 固定的最小值 和 固定的最大值 来表示的）
-    "variable": {
-        data_type.Mysql5p7.CHAR: (0, (1 << 8) - 1),
-        data_type.Mysql5p7.VARCHAR: (0, (1 << 16) - 1),
-
-        data_type.Mysql5p7.TINYBLOB: (0, (1 << 8) - 1),
-        data_type.Mysql5p7.BLOB: (0, (1 << 16) - 1),
-        data_type.Mysql5p7.MEDIUMBLOB: (0, (1 << 24) - 1),
-        data_type.Mysql5p7.LONGBLOB: (0, (1 << 32) - 1),
-
-        data_type.Mysql5p7.TINYTEXT: (0, (1 << 8) - 1),
-        data_type.Mysql5p7.TEXT: (0, (1 << 16) - 1),
-        data_type.Mysql5p7.MEDIUMTEXT: (0, (1 << 24) - 1),
-        data_type.Mysql5p7.LONGTEXT: (0, (1 << 32) - 1),
-
-    },
+    variable = None
     # 特殊的可变长度的数据类型
-    "special-variable": {
-        data_type.Mysql5p7.DECIMAL: ((1, 65), (0, 30)),
-        # data_type.Mysql5p7.DEC: ((1, 65), (0, 30)),
-    },
+    special_variable = None
 
-}
+    def to_json_string(self):
+        return json.dumps(self, cls=MyJSONEncoder, sort_keys=True, indent=4)
+
+
+# mysql 5.7的数据类型长度范围，长度单位： Byte
+class MySQL5p7(BaseStruct):
+    def __init__(self):
+        # 固定长度的数据类型
+        self.fixed = {
+            data_type.Mysql5p7.TINYINT: 1,
+            data_type.Mysql5p7.SMALLINT: 2,
+            data_type.Mysql5p7.MEDIUMINT: 3,
+            data_type.Mysql5p7.INTEGER: 4,
+            # data_type.Mysql5p7.INT: 4,
+            data_type.Mysql5p7.BIGINT: 8,
+
+            data_type.Mysql5p7.FLOAT: 4,
+            data_type.Mysql5p7.DOUBLE: 8,
+
+            data_type.Mysql5p7.DATE: 3,
+            data_type.Mysql5p7.TIME: 3,
+            data_type.Mysql5p7.YEAR: 1,
+            data_type.Mysql5p7.DATETIME: 8,
+            data_type.Mysql5p7.TIMESTAMP: 4,
+        }
+        # 可变长度的数据类型（范围可以用 固定的最小值 和 固定的最大值 来表示的）
+        self.variable = {
+            data_type.Mysql5p7.CHAR: (0, (1 << 8) - 1),
+            data_type.Mysql5p7.VARCHAR: (0, (1 << 16) - 1),
+
+            data_type.Mysql5p7.TINYBLOB: (0, (1 << 8) - 1),
+            data_type.Mysql5p7.BLOB: (0, (1 << 16) - 1),
+            data_type.Mysql5p7.MEDIUMBLOB: (0, (1 << 24) - 1),
+            data_type.Mysql5p7.LONGBLOB: (0, (1 << 32) - 1),
+
+            data_type.Mysql5p7.TINYTEXT: (0, (1 << 8) - 1),
+            data_type.Mysql5p7.TEXT: (0, (1 << 16) - 1),
+            data_type.Mysql5p7.MEDIUMTEXT: (0, (1 << 24) - 1),
+            data_type.Mysql5p7.LONGTEXT: (0, (1 << 32) - 1),
+        }
+        # 特殊的可变长度的数据类型
+        self.special_variable = {
+            data_type.Mysql5p7.DECIMAL: ((1, 65), (0, 30)),
+            # data_type.Mysql5p7.DEC: ((1, 65), (0, 30)),
+        }
+
+
+# 单例
+mysql5p7 = MySQL5p7()
