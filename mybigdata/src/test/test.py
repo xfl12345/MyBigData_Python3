ab65f508b7f191ca275cc1509bc6a49e9bb671c7
xfl12345 <xfl1046539849@qq.com>
2021-06-22 20:21:41 +0800

    测试自定义JSON编码器

---
+++ mybigdata/src/test/test.py
@@ -0,0 +1,8 @@
+import mybigdata.src.main.appconst.data_type_lenght as data_type_lenght
+import mybigdata.src.main.appconst.data_type_class as data_type_class
+
+print(data_type_lenght.mysql5p7.to_json_string())
+print()
+print(data_type_class.mysql5p7.to_json_string())
+
+
