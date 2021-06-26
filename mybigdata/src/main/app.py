a8cc3d7931aff41d6c118b39609e454227df4b25
xfl12345 <xfl1046539849@qq.com>
2021-06-26 21:48:10 +0800

    添加注释 & 暂时绑定根目录到 连接池监视器

--- mybigdata/src/main/app.py
+++ mybigdata/src/main/app.py
@@ -6,13 +6,9 @@ import flask_restful
 
 from mybigdata.src.main.db_monitor import Monitor
 
-#     monitor_class = monitor()
-#     monitor_thread = threading.Thread(target=monitor_class.run,)
-#     monitor_thread.start()
-
-
 if __name__ == "__main__":
     # source code URL=https://www.jianshu.com/p/6ac1cab17929
+    # source code URL=https://flask-restful.readthedocs.io/en/latest/quickstart.html
     app = flask.Flask("MyBigData")
     app.json_encoder.ensure_ascii = False
     app.config["JSON_AS_ASCII"] = False
@@ -20,6 +16,10 @@ if __name__ == "__main__":
     app.config["JSONIFY_MIMETYPE"] = "application/json;charset=UTF-8"
     api = flask_restful.Api(app)
 
-    api.add_resource(Monitor, "/monitor")
+    # 添加 Monitor 监视器到路由
+    api.add_resource(Monitor, "/monitor", "/")
 
+
+
+    # 启用Flask框架的调试模式（可以热重新加载Python源码）
     app.run(debug=True)
