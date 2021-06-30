2385148ab2882045ac3457af50bdbf4188249ce5
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2021-06-30 09:17:49 +0800

    修复路径错误

--- mybigdata/src/test/test_json_schema.py
+++ mybigdata/src/test/test_json_schema.py
@@ -4,12 +4,11 @@ import jschon
 
 print(os.getcwd())
 
-with open("src/test/test.json", "r", encoding="utf-8") as f:
+with open("mybigdata/src/test/test.json", "r", encoding="utf-8") as f:
     testJsonStr = json.loads(f.read())
 print(testJsonStr)
 
-
-with open("src/main/resources/json/schema/base_request_object.json", "r", encoding="utf-8") as f:
+with open("mybigdata/src/main/resources/json/schema/base_request_object.json", "r", encoding="utf-8") as f:
     jsonSchemaStr = json.loads(f.read())
 print(jsonSchemaStr)
 
@@ -19,6 +18,3 @@ jsonObject = jschon.JSON(testJsonStr)
 
 print( jsonObject )
 print(apiJsonSchema.evaluate( jsonObject ).valid)
-
-
-
