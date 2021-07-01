b72d6700bcbc01ef88079cf4747b18ca7effcb73
xfl12345 <xfl1046539849@qq.com>
2021-07-01 18:28:56 +0800

    使用绝对地址来彻底修复资源路径不正确的问题

--- mybigdata/src/test/test_json_schema.py
+++ mybigdata/src/test/test_json_schema.py
@@ -2,13 +2,15 @@ import json
 import os
 import jschon
 
+root_path = os.path.abspath(os.path.dirname(__file__)).split("mybigdata")[0]
+print(root_path)
 print(os.getcwd())
 
-with open("mybigdata/src/test/test.json", "r", encoding="utf-8") as f:
+with open(root_path + "mybigdata/src/test/test.json", "r", encoding="utf-8") as f:
     testJsonStr = json.loads(f.read())
 print(testJsonStr)
 
-with open("mybigdata/src/main/resources/json/schema/base_request_object.json", "r", encoding="utf-8") as f:
+with open(root_path + "mybigdata/src/main/resources/json/schema/base_request_object.json", "r", encoding="utf-8") as f:
     jsonSchemaStr = json.loads(f.read())
 print(jsonSchemaStr)
 
