c390f2ea440ac035229dccfe07bc6da53f32b60e
xfl12345 <xfl1046539849@qq.com>
2021-07-11 22:58:00 +0800

    尝试设计 JSON -> Python自定义类 的工具

---
+++ mybigdata/src/main/model/utils/json_utils.py
@@ -0,0 +1,20 @@
+import json
+
+def json2class(target_object:object, json_content):
+    json_in_dict = {}
+    if isinstance(json_content, str):
+        json_in_dict = json.loads(json_content)
+    elif not isinstance(json_content, dict):
+        raise TypeError("Unexcepted args input.")
+
+    return dict2class(target_object=target_object, source_dict=json_in_dict)
+
+
+def dict2class(target_object:object, source_dict:dict):
+    if not isinstance(source_dict, dict):
+        raise TypeError("Unexcepted args input.")
+
+    for key in source_dict.keys():
+        if key in target_object.__dict__.keys():
+            # TODO 判断是否是一样的数据类型
+            target_object.__setattr__(key, source_dict[key])
