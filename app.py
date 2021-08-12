452cc85e2c079ce53e97c7b5667f7a645f0b796f
xfl12345 <xfl1046539849@qq.com>
2021-08-12 23:32:35 +0800

    注释掉一行没有用的代码，因为json_encoder在后面都被替换成自定义类了……

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     # source code URL=https://www.jianshu.com/p/6ac1cab17929
     # source code URL=https://flask-restful.readthedocs.io/en/latest/quickstart.html
     app = flask.Flask("MyBigData")
-    app.json_encoder.ensure_ascii = False
     app.config["JSON_AS_ASCII"] = False
+    # app.json_encoder.ensure_ascii = False
     app.json_encoder = MyJSONEncoder
     app.config["JSONIFY_MIMETYPE"] = "application/json;charset=UTF-8"
     api = flask_restful.Api(app)
